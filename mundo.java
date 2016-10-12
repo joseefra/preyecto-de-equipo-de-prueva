0a419cd83f41309a759dc22f9a9484d05fd65b5b
joseefra <vert2jose@gmail.com>
2016-10-12 10:32:39 -0500

    prueva

---
+++ mundo.java
@@ -0,0 +1,21 @@
+import greenfoot.*;
+
+/**
+ * Escribe una descrición de la clase mundo aquí.
+ * 
+ * @autor (tu nombre) 
+ * @versión (Un número de versión o una fecha)
+ */
+public class mundo extends World
+{
+
+    /**
+     * Constructor para objetos de clase mundo.
+     * 
+     */
+    public mundo()
+    {    
+        // Crea un nuevo mundo de 600x400 celdas con un tamaño de celda de 1x1 pixeles.
+        super(600, 400, 1); 
+    }
+}
